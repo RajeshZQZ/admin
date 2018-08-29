f4cec8e7f5ec012acec531790072462cd6d5f943
RajeshZQZ <939546484@qq.com>
2018-08-29 16:03:48 +0800

    222

--- app/init.php
+++ app/init.php
@@ -1,5 +1,5 @@
 <?php
-include (__DIR__ . '/config.php');
-include  __DIR__ . '/global.php'; //引入全局公用方法
-include  __DIR__. '/fromework/base/autoloader.class.php';
+include ('/game01/admin/app/config.php');
+include  '/game01/admin/app/global.php'; //引入全局公用方法
+include  '/game01/admin/app/fromework/base/autoloader.class.php';
 framework_base_autoloader::init();
