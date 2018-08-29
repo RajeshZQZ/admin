b6fb444e99753111b4e60b49e7c2934e29c47cf0
939546484@qq.com <root@rajesh.zqz>
2018-08-30 01:36:14 +0800

    moke

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-require ('/usr/local/httpd/htdocs/init.php');
+require ('/usr/local/httpd/htdocs/init_admin.php');
   doAction();//转至对应方法
 ?>
--- moke/shijijy_test.php
+++ moke/shijijy_test.php
@@ -1,6 +1,6 @@
 <?php
 $_REQUEST['act'] = 'test_dotest';
 $_REQUEST['st'] = 'james';
-require ('/usr/local/httpd/htdocs/init.php');
+require ('/usr/local/httpd/htdocs/init_admin.php');
   doAction();//转至对应方法
 
