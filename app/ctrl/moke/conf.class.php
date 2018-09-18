18b07a81a8db8f6cd28548454659978a8dfe0980
RajeshZQZ <939546484@qq.com>
2018-09-19 05:23:35 +0800

    err

--- app/ctrl/moke/conf.class.php
+++ app/ctrl/moke/conf.class.php
@@ -7,33 +7,26 @@
  * Time: 15:49
  */
 
-class ctrl_moke_conf{
+class ctrl_moke_conf
+{
 
-public function input(){
-    $date= array(
-    'name' => $_REQUEST['name'],
-    'type' => $_REQUEST['type'],
-    'url' => $_REQUEST['url'],
-    'check_url' => $_REQUEST['check_url'],
-    'Interface_array' => $_REQUEST['Interface_array'],
-   // $Interface_array = explode(',', $_REQUEST('Interface_array')),
-    )
+    public function input()
+    {
+        $date = array(
+            'name' => $_REQUEST['name'],
+            'type' => $_REQUEST['type'],
+            'url' => $_REQUEST['url'],
+            'check_url' => $_REQUEST['check_url'],
+            'Interface_array' => $_REQUEST['Interface_array'],
+        )
+   // $Interface_array = explode(',', $_REQUEST('Interface_array'))
     $res = model_moke_conf::insert($date);
-if (!empty($res)){
-echo "插入成功~！";
-}else{
+if (!empty($res)) {
+    echo "插入成功~！";
+} else {
     echo "插入失败~！";
 }
+    }
 
 }
 
-
-
-
-
-
-
-
-
-
-}
