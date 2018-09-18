59d2b778e07a248cf57fa06435dc689ee9da74fe
RajeshZQZ <939546484@qq.com>
2018-09-19 05:01:13 +0800

    err

---
+++ app/ctrl/moke/conf.class.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: zengqingzhang
+ * Date: 2018/9/19
+ * Time: 15:49
+ */
+
+class ctrl_moke_conf{
+
+public function input(){
+    $date= array(
+    $name = $_REQUEST('name'),
+    $type = $_REQUEST('type'),
+    $url = $_REQUEST('url'),
+    $check_url = $_REQUEST('check_url'),
+    $Interface_array = $_REQUEST('Interface_array')  ,
+   // $Interface_array = explode(',', $_REQUEST('Interface_array')),
+    )
+    $res = model_moke_conf::insert($date);
+if (!empty($res)){
+echo "插入成功~！";
+}else{
+    echo "插入失败~！";
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+}
---
+++ app/model/moke/conf.class.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: zengqingzhang
+ * Date: 2018/9/19
+ * Time: 15:49
+ */
+
+class model_moke_conf extends model_base
+{
+
+    public function insert($data){
+        $db = $this ->connect_db();
+        $sql = "INSERT INTO moketest_config(name,type,url,check_url,Interface_array,raw_add_time) VALUES(?,?,?,?,?,?);";
+        $stmt = $db->prepare($sql);
+        $stmt->bind_param("ssssss",$data['$name'],$data['$type'],$data['$url'],$data['$check_url'],$data['$Interface_array'],now());
+        $stmt->execute();
+        $this ->close_mysql($db);
+        return $res = $stmt->affected_rows;
+    }
+}
+
