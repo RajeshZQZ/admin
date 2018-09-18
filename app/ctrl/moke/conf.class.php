bdc1f9bd4770b1a73e82adda52cdb4e4272cfeee
RajeshZQZ <939546484@qq.com>
2018-09-19 05:19:40 +0800

    err

--- app/ctrl/moke/conf.class.php
+++ app/ctrl/moke/conf.class.php
@@ -11,11 +11,11 @@ class ctrl_moke_conf{
 
 public function input(){
     $date= array(
-    $name = $_REQUEST('name'),
-    $type = $_REQUEST('type'),
-    $url = $_REQUEST('url'),
-    $check_url = $_REQUEST('check_url'),
-    $Interface_array = $_REQUEST('Interface_array')  ,
+    'name' => $_REQUEST['name'],
+    'type' => $_REQUEST['type'],
+    'url' => $_REQUEST['url'],
+    'check_url' => $_REQUEST['check_url'],
+    'Interface_array' => $_REQUEST['Interface_array'],
    // $Interface_array = explode(',', $_REQUEST('Interface_array')),
     )
     $res = model_moke_conf::insert($date);
--- app/model/moke/conf.class.php
+++ app/model/moke/conf.class.php
@@ -12,9 +12,9 @@ class model_moke_conf extends model_base
 
     public function insert($data){
         $db = $this ->connect_db();
-        $sql = "INSERT INTO moketest_config(name,type,url,check_url,Interface_array,raw_add_time) VALUES(?,?,?,?,?,?);";
+        $sql = "INSERT INTO moketest_config(name,typ,url,check_url,Interface_array,raw_add_time) VALUES(?,?,?,?,?,?);";
         $stmt = $db->prepare($sql);
-        $stmt->bind_param("ssssss",$data['$name'],$data['$type'],$data['$url'],$data['$check_url'],$data['$Interface_array'],now());
+        $stmt->bind_param("ssssss",$data['name'],$data['type'],$data['url'],$data['check_url'],$data['Interface_array'],now());
         $stmt->execute();
         $this ->close_mysql($db);
         return $res = $stmt->affected_rows;
