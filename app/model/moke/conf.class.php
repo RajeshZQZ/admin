4da4984d4bef7245eb5c5f1d0d0e2a03964d1f87
RajeshZQZ <939546484@qq.com>
2018-09-20 04:02:49 +0800

    err

--- app/model/moke/conf.class.php
+++ app/model/moke/conf.class.php
@@ -11,12 +11,21 @@ class model_moke_conf extends model_base
 {
 
     public function insert($data){
+        echo json_encode($data)."<br>";
         $dba = parent::connect_db();
         $sql = "INSERT INTO moketest_config(name,typ,url,check_url,Interface_array,raw_add_time) VALUES(?,?,?,?,?,?);";
         $stmt = $dba->prepare($sql);
         //用变量绑定?表示的值,i表示整型,d表示浮点型,b代表二进制,s代表其它的所有
-        $stmt->bind_param("sissss",$data['name'],$data['type'],$data['url'],$data['check_url'],$data['Interface_array'],'2018-09-18 03:50:36');
+        $name = $data['name'];
+        $tpye = $data['type'];
+        $url  = $data['url'];
+        $check_url = $data['check_url'];
+        $Interface_array = $data['Interface_array'];
+        $time = '2018-09-18 03:50:36';
+        $stmt->bind_param("sissss",$name,$tpye,$url,$check_url,$Interface_array,$time);
+        echo "准备执行~!";
         $res = $stmt->execute();
+        echo $res ;
         parent::close_mysql($dba);
         return $res;
     }
