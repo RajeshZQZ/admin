5fe8e4f0df45729a951b3bf2d858e65e32cf3066
RajeshZQZ <939546484@qq.com>
2018-09-20 01:21:35 +0800

    err

--- app/ctrl/moke/conf.class.php
+++ app/ctrl/moke/conf.class.php
@@ -6,13 +6,15 @@
  * Date: 2018/9/19
  * Time: 15:49
  */
-require_once APP_DIR.'model/conf.class.php';
+//require_once APP_DIR.'model/conf.class.php';
 class ctrl_moke_conf
 {
     public function input()
     {
-        $date = array(
-            'name' => $_REQUEST['name'],
+        $date = $_POST;
+        echo $_POST;
+        $pare = array(
+            'name' => $_POST['name'],
             'type' => $_REQUEST['type'],
             'url' => $_REQUEST['url'],
             'check_url' => $_REQUEST['check_url'],
