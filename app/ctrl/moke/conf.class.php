8a025b91cf0023f9f0088043a8000e05075f05ce
RajeshZQZ <939546484@qq.com>
2018-09-20 03:47:48 +0800

    err

--- app/ctrl/moke/conf.class.php
+++ app/ctrl/moke/conf.class.php
@@ -13,18 +13,18 @@ class ctrl_moke_conf
     {
         $date = $_POST;
         echo  json_encode($_POST);
-      //  $pare = array(
-      //      'name' => $_POST['name'],
-      //      'type' => $_REQUEST['type'],
-      //      'url' => $_REQUEST['url'],
-      //     'check_url' => $_REQUEST['check_url'],
-     //       'Interface_array' => $_REQUEST['Interface_array'] )
-    $res = model_moke_conf::test();
-//    $res = $db->insert($date);
-        if (!empty($res)){
-            echo "OKOKOKOK`~~~".var_dump($res);
+      $pare = array(
+           'name' => $_POST['name'],
+           'type' => $_POST['type'],
+           'url' => $_POST['url'],
+           'check_url' => $_POST['check_url'],
+           'Interface_array' => $_POST['Interface_array'] );
+  //  $res = model_moke_conf::test();
+        $res = model_moke_conf::insert($pare);
+        if (!$res){
+            echo "<br>数据插入失败~！<br>";
         }else{
-            echo "NONONONONO~!~";
+            echo "<br>数据插入成功~！<br>";
         }
 /*/if (!empty($res)) {
   //  echo "插入成功~！";
--- app/model/moke/conf.class.php
+++ app/model/moke/conf.class.php
@@ -1,5 +1,5 @@
 <?php
-require_once APP_DIR.'model/base.class.php';
+//require_once APP_DIR.'model/base.class.php';
 /**
  * Created by PhpStorm.
  * User: zengqingzhang
@@ -11,13 +11,14 @@ class model_moke_conf extends model_base
 {
 
     public function insert($data){
-        $db = parent::connect_db();
+        $dba = parent::connect_db();
         $sql = "INSERT INTO moketest_config(name,typ,url,check_url,Interface_array,raw_add_time) VALUES(?,?,?,?,?,?);";
-        $stmt = $db->prepare($sql);
-        $stmt->bind_param("ssssss",$data['name'],$data['type'],$data['url'],$data['check_url'],$data['Interface_array'],'2018-09-18 03:50:36');
-        $stmt->execute();
-        $this ->close_mysql($db);
-        return $res = $stmt->affected_rows;
+        $stmt = $dba->prepare($sql);
+        //用变量绑定?表示的值,i表示整型,d表示浮点型,b代表二进制,s代表其它的所有
+        $stmt->bind_param("sissss",$data['name'],$data['type'],$data['url'],$data['check_url'],$data['Interface_array'],'2018-09-18 03:50:36');
+        $res = $stmt->execute();
+        parent::close_mysql($dba);
+        return $res;
     }
 
     public function test(){
