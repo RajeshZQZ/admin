94ea5d70235286c92a7c7f58da1d9e679573450c
zengqingzhang <zengqingzhang@credit91.com>
2019-10-24 15:53:37 +0800

    index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-//require ('/usr/local/httpd/htdocs/init_admin.php');
-  //doAction();//转至对应方法
-header('Location: http://www.baidu.com/');
+require ('/usr/local/httpd/htdocs/init_admin.php');
+doAction();//转至对应方法
+//header('Location: http://www.baidu.com/');
 
 ?>
